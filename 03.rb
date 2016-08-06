4e2e60a05c0cb2109a82d80720764e481f648e01
Xia Kai(夏恺) <xiaket@gmail.com>
2016-08-06 15:50:49 +1000

    should pick number from prime_division's result.
    Signed-off-by: Kent Xia <kentx@pronto.net>

--- 03.rb
+++ 03.rb
@@ -2,11 +2,11 @@
 # Author:         Kent Xia/Xia Kai <kentx@pronto.net/xiaket@gmail.com>
 # Filename:       03.rb
 # Date created:   2016-08-06 15:17
-# Last modified:  2016-08-06 15:19
+# Last modified:  2016-08-06 15:50
 #
 # Description:
 #
 
 require 'prime'
 
-puts 600851475143.prime_division.flatten.sort[-1]
+puts 600851475143.prime_division.map { |i| i[0] }[-1]
