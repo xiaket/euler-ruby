7a007bec4de5f48d445ae26f6e3c42f099a590f6
Xia Kai(夏恺) <xiaket@gmail.com>
2016-08-06 16:33:16 +1000

    solved 07
    Signed-off-by: Kent Xia <kentx@pronto.net>

---
+++ 07.rb
@@ -0,0 +1,11 @@
+# encoding: UTF-8
+# Author:         Kent Xia/Xia Kai <kentx@pronto.net/xiaket@gmail.com>
+# Filename:       07.rb
+# Date created:   2016-08-06 16:32
+# Last modified:  2016-08-06 16:32
+#
+# Description:
+#
+require 'prime'
+
+puts Prime.first(10001)[-1]
